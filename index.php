3c9718c8ea83c407d4db6755780e4b86cebaf61f
GeniusHCMUS <minhluan2292@gmail.com>
2017-06-03 15:52:13 +0700

    Update UI Login

--- index.php
+++ index.php
@@ -4,30 +4,76 @@
 <head>
     <meta charset="utf-8" />
     <title><?php siteName(); ?> | <?php pageTitle(); ?></title>
-    <style type="text/css">
-        .wrap { max-width: 700px; margin: 50px auto; padding: 30px; text-align: center; box-shadow: 0 0 5px rgba(0,0,0,.5); }
-        article { text-align: left; padding: 40px; }
-    </style>
+      <style type="text/css">
+      .bgimg-1 {
+           background-image: url("img/bg2.jpg");
+           height: 100%;
+          }
+    .page-signin-modal {
+      position: relative;
+      top: auto;
+      right: auto;
+      bottom: auto;
+      left: auto;
+      z-index: 1;
+      display: block;
+    }
+    .page-signin-form-group { position: relative; }
+    .page-signin-icon {
+      position: absolute;
+      line-height: 21px;
+      width: 36px;
+      border-color: rgba(0, 0, 0, .14);
+      border-right-width: 1px;
+      border-right-style: solid;
+      left: 1px;
+      top: 9px;
+      text-align: center;
+      font-size: 15px;
+    }
+    html[dir="rtl"] .page-signin-icon {
+      border-right: 0;
+      border-left-width: 1px;
+      border-left-style: solid;
+      left: auto;
+      right: 1px;
+    }
+    html:not([dir="rtl"]) .page-signin-icon + .page-signin-form-control { padding-left: 50px; }
+    html[dir="rtl"] .page-signin-icon + .page-signin-form-control { padding-right: 50px; }
+    #page-signin-forgot-form {
+      display: none;
+    }
+    .page-signin-modal > .modal-dialog { margin: 30px 10px; }
+    @media (min-width: 544px) {
+      .page-signin-modal > .modal-dialog { margin: 60px auto; }
+    }
+  </style>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" class="px-demo-stylesheet-bs">
+    <link href="css/pixeladmin.min.css" rel="stylesheet" type="text/css" class="px-demo-stylesheet-core">
+    <link href="css/pixeladmin.min-blessed1b401.css" rel="stylesheet" type="text/css">
+    <link href="css/widgets.min.css" rel="stylesheet" type="text/css">
+    <link href="css/demo.css" rel="stylesheet" type="text/css">
+    <link href="css/adminflare.min.css" rel="stylesheet" type="text/css">
+    <link href="css/font/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/icon/ionicons.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div class="wrap">
-
+<div class="bgimg-1">
     <header>
-        <h2><?php siteName(); ?></h2>
         <nav class="menu">
             <a href="/" title="Home">Home</a> |    
             <a href="/?page=about-us">About</a> |
             <a href="/?page=products">Products</a> |
             <a href="/?page=contact-us">Contact</a>
+            <a href="/?page=login">Login</a>
         </nav>
     </header>    
 
     <article>
-        <h3><?php pageTitle(); ?></h3>
         <?php pageContent(); ?>
     </article>
 
-    <footer><small>&copy;<?php echo date('Y'); ?> <?php siteName(); ?> Luan Huynh</small></footer>
 </div>
 </body>
 </html>
