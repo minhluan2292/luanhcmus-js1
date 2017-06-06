8d908e8eaf50049ca9ac0312401bd3efffada6fc
minhluan2292 <minhluan2292@gmail.com>
2017-06-06 14:24:11 -0400

    test delete

--- pages/readfile.php
+++ pages/readfile.php
@@ -9,8 +9,8 @@ while ($file = readdir($dir)) {
 
   echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
   echo "<a class='btn btn-warning btn-rounded' href='pages/fileshare/$file'><span class='btn-label-icon left fa fa-file'></span>".$file."</a>";
-   echo "<a class='btn btn-danger' href='pages/delete.php?f=$file'><span class='btn-label-icon left fa fa-delete'></span>"."Delete"."</a>";
-    echo "<br>";
+//    echo "<a class='btn btn-danger' href='pages/delete.php?f=$file'><span class='btn-label-icon left fa fa-delete'></span>"."Delete"."</a>";
+//     echo "<br>";
 }
 closedir($dir);
 ?>
